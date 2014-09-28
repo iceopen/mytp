aef4f24fb9ed05d63496dd89e0ae1cf1db1e2f8a
iceinto <iceinto@163.com>
2014-09-28 09:19:19 +0800

    修改默认logs目录

--- code/i.php
+++ code/i.php
@@ -1,6 +1,7 @@
 <?php
-define('RUNTIME_PATH', SITE_PATH.'/cache/'.APP_NAME.'/');
+define('RUNTIME_PATH', SITE_PATH . '/cache/' . APP_NAME . '/');
 define('LOG_PATH', SITE_PATH . '/logs/');
 // 加载框架入口文件
-require(SITE_PATH."/code/ThinkPHP/ThinkPHP.php");
+define("TP_PATH", str_ireplace('\\', '/', dirname(__FILE__)));
+require(TP_PATH . "/ThinkPHP/ThinkPHP.php");
 ?>
